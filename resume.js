60de022673cc03693434f65bdfc93f30f6388bc9
ethanfender1 <ethanfender1@gmail.com>
2025-09-17 09:19:53 -0500

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,13 @@
+// Define Variables
+let myName = "Ethan Fender";
+let title = "Student Athlete";
+let experience = "Baseball, Business Student, and Strength Training";
+let education = "Oral Roberts University";
+let skills = ["baseball", "hardworking", "communication", ];
+
+//Connect Varibles to the HTML (ligaments)
+document.getElementById("my-name").textContent = myName;
+document.getElementById("my-email").textContent = myEmail;
+document.getElementById("title").textContent = title;
+document.getElementById("experience").textContent = experience;
+document.getElementById("education").textContent = education;
