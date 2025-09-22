346b40da94bd3d46fb2fdb863009ed2f5c521fec
ethanfender1 <ethanfender1@gmail.com>
2025-09-22 10:15:56 -0500

    Update resume.js

--- resume.js
+++ resume.js
@@ -1,9 +1,13 @@
 // Define Variables
 let myName = "Ethan Fender";
+let myEmail = "ethanfender1@gmail.com";
 let title = "Student Athlete";
 let experience = "Baseball, Business Student, and Strength Training";
 let education = "Oral Roberts University";
-let skills = ["baseball", "hardworking", "communication", ];
+let skills = "Energetic, hard working, communication";
+function showFunFact() {
+ alert("Fun Fact: I love to Hunt!");
+}
 
 //Connect Varibles to the HTML (ligaments)
 document.getElementById("my-name").textContent = myName;
@@ -11,3 +15,4 @@ document.getElementById("my-email").textContent = myEmail;
 document.getElementById("title").textContent = title;
 document.getElementById("experience").textContent = experience;
 document.getElementById("education").textContent = education;
+document.getElementById("skills").textContent = skills;
